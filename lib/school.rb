c27c703d01aac827c0faad005b854124eae967cf
Adam <adamadolfo8@gmail.com>
2020-08-29 14:46:20 -0400

    incomplete last test nds

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+class School
+
+    attr_accessor :roster, :grade
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+
+    end
+
+    def add_student(name, grade)
+       if @roster[grade]
+        @roster[grade] << name
+       else 
+        @roster[grade] = []
+        @roster[grade] << name
+       end
+    end
+
+    def grade(grade)
+        self.roster[grade]
+    end
+    
+    def sort 
+        @roster.each do |grade|
+            #### incomplete here
+        end
+    
+
+    end
+
+
+
+end
